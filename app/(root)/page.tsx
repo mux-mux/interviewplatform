431f758b5dea618aa06bb3caf1a96a61a96bb1af
mux-mux <kobylyukh@gmail.com>
2025-07-17 09:59:29 +0300

    add basic homepage markup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,40 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Page = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        ></Image>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section"></div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Inreview</h2>
+        <div className="interviews-section">
+          <p>There are no interviews available</p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Page;
