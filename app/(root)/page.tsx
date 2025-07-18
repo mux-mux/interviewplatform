870efa4f5cb9372223895dc84a9f3ded5f32b708
mux-mux <kobylyukh@gmail.com>
2025-07-18 10:45:18 +0300

    add home page markup and styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Image from 'next/image';
+import { dummyInterviews } from '@/constants';
+import InterviewCard from '@/components/InterviewCard';
 
 const Page = () => {
   return (
     <>
       <section className="card-cta">
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 max-w-lg">
           <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
           <p className="text-lg">
             Practice on real interview questions & get instant feedback
@@ -23,14 +25,22 @@ const Page = () => {
           className="max-sm:hidden"
         ></Image>
       </section>
-      <section className="flex flex-col gap-6 mt-8">
+      <section className="flex flex-col gap-6 mt-8 card-cta">
         <h2>Your Interviews</h2>
-        <div className="interviews-section"></div>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
       </section>
-      <section className="flex flex-col gap-6 mt-8">
+      <section className="flex flex-col gap-6 mt-8 card-cta">
         <h2>Take an Inreview</h2>
         <div className="interviews-section">
-          <p>There are no interviews available</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+
+          {/* <p>You haven&apos;t taken any interviews yet</p> */}
         </div>
       </section>
     </>
