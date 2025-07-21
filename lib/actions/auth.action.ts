119f8ec99336e830a624fa7b4a223489b9de30cf
mux-mux <kobylyukh@gmail.com>
2025-07-21 15:34:22 +0300

    add signUp firebase action

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,46 @@
+'use server';
+
+import { db } from '@/firebase/admin';
+
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+
+  try {
+    const userRecord = await db.collection('users').doc(uid).get();
+
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: 'User already exists. Please sign in instead.',
+      };
+    }
+
+    await db.collection('users').doc(uid).set({
+      name,
+      email,
+    });
+  } catch (error: unknown) {
+    console.error('Error creating a user', error);
+
+    if (isAuthError(error) && error.code === 'auth/email-already-exists') {
+      return {
+        success: false,
+        message: 'This email is already in use.',
+      };
+    }
+
+    return {
+      success: false,
+      message: 'Failed to create an account',
+    };
+  }
+}
+
+function isAuthError(error: unknown): error is { code: string } {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'code' in error &&
+    (error as { code: unknown }).code === 'auth/email-already-exists'
+  );
+}
