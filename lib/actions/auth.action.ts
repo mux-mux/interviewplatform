e925733eda280608dd4d1663d715a16b8e05c679
mux-mux <kobylyukh@gmail.com>
2025-07-29 14:29:58 +0300

    add signIn session handler

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -84,6 +84,42 @@ export async function setSessionCookie(idToken: string) {
   });
 }
 
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get('session')?.value;
+
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    const userRecord = await db
+      .collection('users')
+      .doc(decodedClaims.uid)
+      .get();
+
+    if (userRecord.exists) {
+      return null;
+    }
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+
+  return !!user;
+}
+
 function isAuthError(error: unknown): error is { code: string } {
   return (
     typeof error === 'object' &&
