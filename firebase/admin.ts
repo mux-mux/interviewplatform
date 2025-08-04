56edb9dc90d010c71d0972bb8f09ee916387e17f
mux-mux <kobylyukh@gmail.com>
2025-08-04 13:58:56 +0300

    fix deployment error

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -4,16 +4,13 @@ import { getFirestore } from 'firebase-admin/firestore';
 
 const initFirebaseAdmin = () => {
   if (!getApps().length) {
-    initializeApp(
-      {
-        credential: cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        }),
-      },
-      'InterviewPlatform'
-    );
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+    });
   }
 
   return {
