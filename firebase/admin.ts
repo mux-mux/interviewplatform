985fcc6ee82e360cae85d1b2a379d263b32b6e5e
mux-mux <kobylyukh@gmail.com>
2025-08-04 13:14:06 +0300

    add initial voice API config

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -3,16 +3,17 @@ import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
 const initFirebaseAdmin = () => {
-  const apps = getApps;
-
-  if (!apps.length) {
-    initializeApp({
-      credential: cert({
-        projectId: process.env.FIREABASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    });
+  if (!getApps().length) {
+    initializeApp(
+      {
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        }),
+      },
+      'InterviewPlatform'
+    );
   }
 
   return {
